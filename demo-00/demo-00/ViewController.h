7ba050dc5eed664d1b393b9c092ba171f3d5ad12
逗叔 <yfb1004@163.com>
2015-08-26 23:52:54 +0800

    项目初始化

---
+++ demo-00/demo-00/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo-00
+//
+//  Created by 逗叔 on 15/8/26.
+//  Copyright (c) 2015年 逗叔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo-00/demo-00/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo-00
+//
+//  Created by 逗叔 on 15/8/26.
+//  Copyright (c) 2015年 逗叔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
